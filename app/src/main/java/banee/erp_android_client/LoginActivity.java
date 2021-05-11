1bf8581e4005a0f6fafcf9a0547ea063dcbe39c1
Banee Ishaque K <baneeishaque2@gmail.com>
2021-05-11 06:52:32 +0000

    Login Actions

--- app/src/main/java/banee/erp_android_client/LoginActivity.java
+++ app/src/main/java/banee/erp_android_client/LoginActivity.java
@@ -2,13 +2,27 @@ package banee.erp_android_client;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Context activityContext = this;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button buttonLogin = findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(v -> {
+
+            startActivity(new Intent(activityContext, DashboardActivity.class));
+            ((AppCompatActivity) activityContext).finish();
+        });
     }
 }
